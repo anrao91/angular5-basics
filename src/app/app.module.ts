5ffdabdfe192a289600184fab381c26224f8b3c8
Anusha Rao <anusha91rao@gmail.com>
2018-04-24 17:47:33 +0530

    Add success and warning alert components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component';
+import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
